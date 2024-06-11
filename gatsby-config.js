4630e96c6e46d267f340c98d4411fbf0f2f48210
Yuri M <yurisdraughts@gmail.com>
2024-06-11 10:57:56 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Матвеев Юрий : фронтенд-разработчик`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
